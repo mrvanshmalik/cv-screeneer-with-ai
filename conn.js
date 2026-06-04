da7934e6c3a9bedfcf3f6cf7ca9948053ccd53f1
mrvanshmalik <malikvansh2704@gmail.com>
2026-06-04 22:14:09 +0530

    backend work

---
+++ conn.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://malikvansh2704_db_user:VchAe0bfXT6pRERf@cluster0.fljdeed.mongodb.net/?appName=Cluster0').then((res)=>{
+    console.log("Data base connected !!!")
+}).catch(err => {
+    console.log("Something err", err);
+})
+
+
+
+
